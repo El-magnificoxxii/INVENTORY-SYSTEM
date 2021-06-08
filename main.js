9f090a4f21628ba37593927d707570a6138f0cdc
El-magnificoxxii <75856679+El-magnificoxxii@users.noreply.github.com>
2021-06-08 14:42:31 +0100

    added delete button

--- main.js
+++ main.js
@@ -9,10 +9,11 @@ var total = document.getElementById('total');
 
 
 
-//EVENT LISTENER
-button.addEventListener("click", runEvent);
-document.addEventListener("DOMContentLoaded", getTasks);
 
+//EVENT LISTENER
+button.addEventListener("click", runEvent);//adding items to inventory
+document.addEventListener("DOMContentLoaded", getTasks);//for attaching to DOM
+inventories.addEventListener("click", removeItem);// for removing items from the table
 //RUN EVENT
 function runEvent(e) {
   e.preventDefault();
@@ -30,18 +31,9 @@ localStorage.setItem('readData', JSON.stringify(formData));
 //saving current and previous data to local storage
 storeTaskInLocalStorage(formData);
 updateSubTotal();
+//updateItem();
 
 
-
-/*
-
-  if (formData != storeTaskInLocalStorage()) {
-    var tr = document.createElement("tr");
-    total.appendChild(tr);
-    tr.appendChild(document.createTextNode("TOTAL"));
-    }
-
-*/
 }
 //GET TASK FROM LOCAL STORAGE AND ATTACHING TO DOM
 function getTasks() {
@@ -56,6 +48,9 @@ function getTasks() {
 
   taskList.forEach(function(task){
       var tr = document.createElement("tr");
+      var btn = document.createElement("button");
+      btn.className = "btn btn-danger btn-sm delete";
+      btn.appendChild(document.createTextNode("X"));
       inventories.appendChild(tr);
         cell1 = tr.insertCell(0);
         cell1.appendChild(document.createTextNode(task["product"]));
@@ -67,6 +62,9 @@ function getTasks() {
         cell4 = tr.insertCell(3);
         cell4.appendChild(document.createTextNode(task["amount"]));
         //console.log(cell3);
+        cell4.appendChild(document.createTextNode(`${quantity.value * pricePerQty.value}`));
+        cell5 = tr.insertCell(4);
+        cell5.appendChild(btn);
 
   });
 
@@ -79,16 +77,21 @@ function getTasks() {
 
 function addRecord(item) {
   var tr = document.createElement("tr");
+  var btn = document.createElement("button");
+  btn.className = "btn btn-danger btn-sm delete";
+  btn.appendChild(document.createTextNode("X"));
   inventories.appendChild(tr);
     cell1 = tr.insertCell(0);
     cell1.appendChild(document.createTextNode(product.value));
     cell2 = tr.insertCell(1);
     cell2.appendChild(document.createTextNode(pricePerQty.value));
     cell3 = tr.insertCell(2);
-    cell3.className = "loop";
     cell3.appendChild(document.createTextNode(quantity.value));
     cell4 = tr.insertCell(3);
     cell4.appendChild(document.createTextNode(`${quantity.value * pricePerQty.value}`));
+    cell5 = tr.insertCell(4);
+    cell5.appendChild(btn);
+
 
 };
 
@@ -119,6 +122,8 @@ function updateSubTotal(item) {
    cell2.appendChild(document.createTextNode(""));
    cell3 = footerRow.insertCell(2);
    cell4 = footerRow.insertCell(3);
+   cell5 = footerRow.insertCell(4);
+   cell5.appendChild(document.createTextNode(""));
 
   let totalForCell3 = Array.from(table.rows).slice(0).reduce((total, row) => {
     return total + parseFloat(row.cells[2].innerHTML);
@@ -130,12 +135,67 @@ function updateSubTotal(item) {
    }, 0);
     cell4.appendChild(document.createTextNode(totalForCell4));
 
+
+
 }
 
+//DELETING A TABLE ROW
+function removeItem(e) {
+  //console.log(e);
+  if (e.target.classList.contains('delete')) {
+    if (confirm("Are you sure ?")) {
+      let row = e.target.parentElement.parentElement;
+      row.parentNode.removeChild(row);
+      //console.log(row);
+      updateSubTotal();
+    }
+  }
+};
+
+
+//UPDATE AMOUNT AND QUANTITY IF ITEM IS THE SAME
 /*
-function onClickRemove(deleteButton) {
-  let row = deleteButton.parentElement.parentElement;
-  row.parentNode.removeChild(row);
-  updateSubTotal(); // Call after delete
-}
+  function updateItem(item) {
+          var table = document.getElementById("inventories");
+          for (var i = 0, row; row = table.rows[i]; i++) {
+            var objCells = table.rows.item(i).cells;
+              console.log(objCells);
+             //iterate through rows
+             //rows would be accessed using the "row" variable assigned in the for loop
+             for (var j = 0, col; col = row.cells[j]; j++) {
+               var sobjCells = row.cells.item[j];
+               console.log(sobjCells);
+               }
+               //iterate through columns
+               //columns would be accessed using the "col" variable assigned in the for loop
+
+               }
+             }
+
+
+             function start() {
+                    var mybody = document.getElementsByTagName("body")[0];
+                    mytable = document.createElement("table");
+                    mytablebody = document.createElement("tbody");
+
+                    for(var row = 0; row < 2; row++) {
+                        mycurrent_row=document.createElement("tr");
+                        for(var col = 0; col < 2; col++) {
+                            mycurrent_cell = document.createElement("td");
+                            currenttext = document.createTextNode("cell is: " + row + col);
+                            mycurrent_cell.appendChild(currenttext);
+                            mycurrent_row.appendChild(mycurrent_cell);
+                            // set the cell background color
+                            // if the column is 0. If the column is 1 hide the cell
+                            if (col === 0) {
+                                mycurrent_cell.style.background = "rgb(255,0,0)";
+                            } else {
+                                mycurrent_cell.style.display = "none";
+                            }
+                        }
+                        mytablebody.appendChild(mycurrent_row);
+                    }
+                    mytable.appendChild(mytablebody);
+                    mybody.appendChild(mytable);
+                 }
 */
